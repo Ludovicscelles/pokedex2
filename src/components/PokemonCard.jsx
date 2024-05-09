545177da9cc57e7af6c40dc4275bfc0b5f6a95f6
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2024-05-09 08:43:21 +0200

    creation of component props.2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,27 @@
-function PokemonCard({pokemon}) {
+import PropTypes from "prop-types";
+
+function PokemonCard({ pokemon }) {
   const message = "Hello Everybody !";
   return (
     <section>
       <figure>
-        {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name}></img> : <p>???</p>}
+        {pokemon.imgSrc ? (
+          <img src={pokemon.imgSrc} alt={pokemon.name}></img>
+        ) : (
+          <p>???</p>
+        )}
         <figcaption>{pokemon.name}</figcaption>
       </figure>
       <p>{message.toUpperCase()}</p>
     </section>
   );
 }
+
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    imgSrc: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default PokemonCard;
